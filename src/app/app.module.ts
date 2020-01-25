e1e8f5f4196cf3166006a66623955371080aac57
Stephanus De Wit <Stephanus@presles.internal>
2020-01-25 13:56:03 +0200

    added capability to capture a image per barcode and to include it in the mail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptFormsModule } from "nativescript-angular/forms"
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BarcodeScanner } from 'nativescript-barcodescanner';
+import { Fontawesome } from "nativescript-fontawesome";
 
 @NgModule({
     bootstrap: [
